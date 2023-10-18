7ca584c3477f7585ff0d5e09cf514d7b8725a060
Taha Gülboy <46195953+jeenklynn@users.noreply.github.com>
2023-10-18 20:14:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("skeleton/head.htm"); ?>
+</head>
+<body>
+    <?php include("skeleton/nav.htm"); ?>
+
+    Content
+
+    <?php include("skeleton/aside.htm"); ?>
+</body>
+    <?php include("skeleton/footer.htm") ?>
+</html>
